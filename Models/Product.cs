12f978446f1a0cfa451eb264a33c23b1b4a938de
Cameron <cameronluyt30@gmail.com>
2021-03-30 17:54:57 +0200

    upload(MuteApi): Project upload

---
+++ Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MuteApi.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal CountInStock { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,81 @@
+using MuteApi.Models;
+using MuteApi.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuteApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Product
+            services.Configure<ProductDatabaseSettings>(
+                Configuration.GetSection(nameof(ProductDatabaseSettings)));
+
+            services.AddSingleton<IProductDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<ProductDatabaseSettings>>().Value);
+
+            // Users
+            services.Configure<UserDatabaseSettings>(
+                Configuration.GetSection(nameof(UserDatabaseSettings)));
+
+            services.AddSingleton<IUserDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<UserDatabaseSettings>>().Value);
+
+            //Services
+            services.AddSingleton<ProductService>();
+            services.AddSingleton<UserService>();
+
+            //Controllers
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MuteApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MuteApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
